ac4e6ad0332f0889245f77cfae568e92a62649cc
AgustinCaldart <caldart123@gmail.com>
2021-05-25 00:38:51 -0300

    Ultimos retoques

--- src/components/allcert.js
+++ src/components/allcert.js
@@ -6,79 +6,59 @@ import cert5 from '../img/certs/fjs.png';
 import cert6 from '../img/certs/frontdevep.png';
 import cert7 from '../img/certs/prework.png';
 import cert8 from '../img/certs/scope.png';
+import cert9 from '../img/certs/react.png';
+import cert10 from '../img/certs/redux.png';
 
 const certs = [
   {
     id: 1,
     image: cert1,
     title: 'Asincronismo con JavaScript',
-    link: 'https://www.facebook.com/',
-    date: '21',
-    month: 'Mar',
-    year: '2020',
   },
   {
     id: 2,
     image: cert2,
     title: 'Básico de JavaScript',
-    link: 'https://www.facebook.com/',
-    date: '09',
-    month: 'Dec',
-    year: '2019',
   },
   {
     id: 3,
     image: cert3,
     title: 'Ecmascript 6+',
-    link: 'https://www.facebook.com/',
-    date: '15',
-    month: 'Jun',
-    year: '2018',
   },
   {
     id: 4,
     image: cert4,
     title: 'Fundamentos de Node.js',
-    link: 'https://www.facebook.com/',
-    date: '04',
-    month: 'Aug',
-    year: '2020',
   },
   {
     id: 5,
     image: cert5,
     title: 'Fundamentos de JavaScritp',
-    link: 'https://www.facebook.com/',
-    date: '09',
-    month: 'Sep',
-    year: '2019',
   },
   {
     id: 6,
     image: cert6,
     title: 'Frontend Developer',
-    link: 'https://www.facebook.com/',
-    date: '09',
-    month: 'Sep',
-    year: '2019',
   },
   {
     id: 7,
     image: cert7,
     title: 'Prework',
-    link: 'https://www.facebook.com/',
-    date: '09',
-    month: 'Sep',
-    year: '2019',
   },
   {
     id: 8,
     image: cert8,
     title: 'Closures y scope en Javascript',
-    link: 'https://www.facebook.com/',
-    date: '09',
-    month: 'Sep',
-    year: '2019',
+  },
+  {
+    id: 9,
+    image: cert9,
+    title: 'Práctuco React',
+  },
+  {
+    id: 10,
+    image: cert10,
+    title: 'React Router y Redux',
   },
 ];
 
